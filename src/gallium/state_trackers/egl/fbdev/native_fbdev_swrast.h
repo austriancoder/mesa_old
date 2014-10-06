ee4b5a4dd59568c078f142f6bc13bf5b94924d0e
Christian Gmeiner <christian.gmeiner@gmail.com>
2014-10-06 16:36:29 +0000

    get rid of this hacky fbdev stuff
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- src/gallium/state_trackers/egl/fbdev/native_fbdev_swrast.h
+++
@@ -1,8 +0,0 @@
-#ifndef __NATIVE_FBDEV_SWRAST_H__
-#define __NATIVE_FBDEV_SWRAST_H__
-
-#include "fbdev/native_fbdev.h"
-
-const struct native_fbdev_driver *swrast_fbdev_get_driver(void);
-
-#endif
--- src/gallium/winsys/etna/fbdev/etna_fbdev_public.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ETNA_FBDEV_PUBLIC_H__
-#define __ETNA_FBDEV_PUBLIC_H__
-
-#include "fbdev/native_fbdev.h"
-
-const struct native_fbdev_driver *etna_fbdev_get_driver(void);
-
-#endif
